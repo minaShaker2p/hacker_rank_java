0e292805501d6b6d8d2a99cb941981ca7f3dd529
Mina Rezkalla <minashakeralfy@gmail.com>
2020-07-22 13:37:08 +0200

    add product class

--- src/com/rezkalla/Main.java
+++ src/com/rezkalla/Main.java
@@ -1,8 +1,14 @@
 package com.rezkalla;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
+        Product door = new Product("wooden door", 35);
+        Product floorPanel = new Product("Floor Panel", 35);
+        Product[] arr = {door, floorPanel};
+        System.out.println(Arrays.toString(arr));
     }
 }
---
+++ src/com/rezkalla/Product.java
@@ -0,0 +1,22 @@
+package com.rezkalla;
+
+import java.util.Arrays;
+
+public class Product {
+    private String name;
+    private Integer price;
+
+    public Product(String name, Integer price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "name='" + name + '\'' +
+                ", price=" + price +
+                '}';
+    }
+
+}
